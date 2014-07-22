57c8054a0d704f6917401944d0a32680ae2defc9
THEVT <rflynn32@gmail.com>
2014-07-21 23:40:11 -0700

    initial commit

---
+++ db/migrate/20130304022201_create_accounts.rb
@@ -0,0 +1,18 @@
+class CreateAccounts < ActiveRecord::Migration
+  def change
+=begin
+    create_table :accounts do |t|
+      t.string :email
+      t.string :pw
+      t.string :tv
+      t.string :session_id
+      t.string :authorization
+      t.integer :enabled
+
+      t.timestamps
+    end
+
+	add_column :accounts, :enabled, :integer, default: 1
+=end
+  end
+end
